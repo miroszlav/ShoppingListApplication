0332609d788453a6cb40c64de8a571b7fa7b8dc7
miroszlav <m.jani@mito.hu>
2017-11-10 13:27:08 +0100

    minor

--- app/src/main/java/hu/miroszlav/shoppinglistapplication/util/AbstractObserver.java
+++ app/src/main/java/hu/miroszlav/shoppinglistapplication/util/AbstractObserver.java
@@ -7,16 +7,14 @@ import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 
-public class AbstractObserver<T> implements Observer<T>{
+public abstract class AbstractObserver<T> implements Observer<T>{
 
     @Override
     public void onSubscribe(@NonNull Disposable d) {
     }
 
     @Override
-    public void onNext(@NonNull T t) {
-
-    }
+    public abstract void onNext(@NonNull T t);
 
     @Override
     public void onError(@NonNull Throwable e) {
